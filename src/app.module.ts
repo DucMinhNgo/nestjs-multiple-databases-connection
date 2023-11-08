3a9151925cde42f781e3f42511e5e9aff71f51d9
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-11-08 14:10:36 +0700

    added user and photo entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { PhotosModule } from './photos/photos.module';
+import { Photo } from './photos/entities/photo.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
+      // name: 'master',
       host: 'localhost',
       port: 3308,
       username: 'root',
       password: 'mypass',
       database: 'dustin_pro',
-      entities: [],
+      entities: [User, Photo],
       synchronize: true,
+      autoLoadEntities: true,
     }),
+    UsersModule,
+    PhotosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
